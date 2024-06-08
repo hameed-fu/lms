8f5d68035b1ff3bd0017b8da7a31190e0047e1b3
Hameed <hameedawkumcs@gmail.com>
2024-06-08 12:34:54 +0500

    first commit

---
+++ backend/parts/sidebar.php
@@ -0,0 +1,14 @@
+<div class="nk-sidebar">           
+            <div class="nk-nav-scroll">
+                <ul class="metismenu" id="menu">
+                    <li class="nav-label">Dashboard</li>
+                     
+
+                    <li><a href="index.php">Dashboard</a></li>
+                    <li><a href="instructor.php">Instructors</a></li>
+                    <li><a href="student.php">Students</a></li>
+                    
+                    
+                </ul>
+            </div>
+        </div>
