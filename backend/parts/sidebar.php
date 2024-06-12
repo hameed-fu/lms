8e3c18a5ee426df98f5d7cbc3c5d4606400eaa98
Hameed <hameedawkumcs@gmail.com>
2024-06-12 13:01:06 +0500

    Sidebar update

--- backend/parts/sidebar.php
+++ backend/parts/sidebar.php
@@ -9,13 +9,14 @@
                     <li><a href="students.php">Students</a></li>
                     <li><a href="courses.php">Courses</a></li>
                     <li><a href="assignements.php">Assignments</a></li>
-                    <li><a href="assignements_submission.php">Assignements submission</a></li>
-                    <li><a href="enrollments.php">enrollment</a></li>
-                    <li><a href="users.php">users</a></li>
+                    <li><a href="assignements_submission.php">Assignements Submission</a></li>
+                    <li><a href="enrollments.php">Enrollment</a></li>
+                    <li><a href="users.php">Users</a></li>
                     <li><a href="notifications.php">Notification</a></li>
                     <li><a href="subject.php">Subjects</a></li>
                     <li><a href="categorey.php">Categorey</a></li>
                     <li><a href="certifications.php">Certificates</a></li>
+                    <li><a href="lectures.php">Lectures</a></li>
                     <li><a href="comments.php">comments</a></li>
                     
                     
