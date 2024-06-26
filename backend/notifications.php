2df741efddfc4aa5f0ff036be931f33a9e03eb1f
paraticasociety <amnacs4334@gmail.com>
2024-06-25 22:34:18 -0700

    notification operation

--- backend/notifications.php
+++ backend/notifications.php
@@ -111,7 +111,7 @@ $result = $conn->query($sql);
                                             <td><?php echo $row['date_created'] ?></td>
                                            <td>
                                                 <a class="btn btn-warning text-white">Edit</a>
-                                                <a class="btn btn-danger text-white">Delete</a>
+                                                <a href="delete_notification.php?=id<?php echo $row['notification_id']?>"class="btn btn-danger text-white">Delete</a>
                                                 
                                             </td>
                                         </tr>
